e9bf6855b31295a69546983219d98fef8f932306
imakiro <imakiro@users.noreply.github.com>
2020-12-02 19:56:50 +0100

    Update install.sh
    retrait install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GREEN='\033[0;32m'
 BLACK='\033[0m'
 
-printf "${GREEN}Installing dependencies with Composer${BLACK}\n"
-cd $SCRIPT_DIR && composer install
+#CF dockerfile qui devrait déja avoir installé les dépendances
+#printf "${GREEN}Installing dependencies with Composer${BLACK}\n"
+#cd $SCRIPT_DIR && composer install
 
 printf "${GREEN}Creating database file${BLACK}\n"
 cd $SCRIPT_DIR && touch "database.sqlite"
